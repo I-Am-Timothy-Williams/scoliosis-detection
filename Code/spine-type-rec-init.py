0fa45322e7175ec7fce2f9a66bae10f156aeb284
Timothy Williams <timwill001@gmail.com>
2023-07-24 18:31:39 -0400

    same as last, forgot to save

--- Code/spine-type-rec-init.py
+++ Code/spine-type-rec-init.py
@@ -54,4 +54,60 @@ def get_data_normal(folder,start,end,label):
     return x_norm_train,y_norm_train
 
 get_data_normal(normal_path,0,train_split_normal,0)
+
+x_scol_train=np.empty((train_split_scol,im_width*im_length))
+y_scol_train=np.empty((train_split_scol,1))
+
+def get_data_scol(folder,start,end,label):
+    file_names = os.listdir(folder)
+    for i in range(start, end):
+        file_path_n = folder + file_names[i]
+        im = tf.io.read_file(file_path_n)
+        im = tf.image.decode_png(im, channels=1)
+        im = tf.image.resize(im, IMAGE_SIZE, method='lanczos5')
+        im_array = asarray(im)
+        x_scol_train[i,:]=im_array.reshape(1,-1)
+        y_scol_train[i,0]=classes[label]
+    return x_scol_train,y_scol_train
+
+get_data_scol(scoliosis_path,0,train_split_scol,1)
         
+
+x_spon_train=np.empty((train_split_spon,im_width*im_length))
+y_spon_train=np.empty((train_split_spon,1))
+
+def get_data_spon(folder,start,end,label):
+    file_names = os.listdir(folder)
+    for i in range(start, end):
+        file_path_n = folder + file_names[i]
+        im = tf.io.read_file(file_path_n)
+        im = tf.image.decode_png(im, channels=1)
+        im = tf.image.resize(im, IMAGE_SIZE, method='lanczos5')
+        im_array = asarray(im)
+        x_spon_train[i,:]=im_array.reshape(1,-1)
+        y_spon_train[i,0]=classes[label]
+    return x_spon_train,y_spon_train
+
+get_data_spon(spondylosis_path,0,train_split_spon,2)
+
+x_train = np.vstack((x_norm_train,x_scol_train,x_spon_train))
+y_train = np.vstack((y_norm_train,y_scol_train,y_spon_train))
+y_train=y_train.ravel()
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(250, 950, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
+
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(x_train,y_train,epochs=10)
---
+++ Code/spine-type-recognition.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 24 17:55:16 2023
+
+@author: Timothy Williams
+"""
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers, models, datasets
+
+img_width = 25
+img_height = 50 #ideally want to have full image res, but computer not strong enough for that
+batch_size = 50
+
+model = models.Sequential()
+model.add(layers.Conv2D(50, (3, 3), activation='relu', input_shape=(25, 50, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(100, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(100, (3, 3), activation='relu'))
+
+model.add(layers.Flatten())
+model.add(layers.Dense(100, activation='relu'))
+model.add(layers.Dense(10))
+
+ds_train = tf.keras.preprocessing.image_dataset_from_directory(
+    'ImagesOriginalSize/',
+    labels = 'inferred',
+    label_mode = "int", #categorical, binary
+    class_names = ['NormalFinal','ScolFinal','SpondFinal'],
+    color_mode = 'grayscale',
+    batch_size = batch_size,
+    image_size = (img_width, img_height), #reshape if not in this size
+    shuffle = True,
+    seed = 123, #makes split same everytime
+    validation_split = 0.1,
+    subset='training')
+
+ds_validation = tf.keras.preprocessing.image_dataset_from_directory(
+    'ImagesOriginalSize/',
+    labels = 'inferred',
+    label_mode = "int", #categorical, binary
+    class_names = ['NormalFinal','ScolFinal','SpondFinal'],
+    color_mode = 'grayscale',
+    batch_size = batch_size,
+    image_size = (img_width, img_height), #reshape if not in this size
+    shuffle = True,
+    seed = 123,
+    validation_split = 0.1,
+    subset='validation')
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(ds_train, epochs = 20, verbose = 2)
+
+
+    
